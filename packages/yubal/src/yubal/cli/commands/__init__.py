6dfef84c7de8fedae04cb203e17c9ff19a5be002
Joe Tan <joetancy@users.noreply.github.com>
2026-09-23 21:02:10 +0800

    feat: export ReplayGain rescan command

--- packages/yubal/src/yubal/cli/commands/__init__.py
+++ packages/yubal/src/yubal/cli/commands/__init__.py
@@ -2,7 +2,14 @@
 
 from yubal.cli.commands.download import download_cmd
 from yubal.cli.commands.meta import meta_cmd
+from yubal.cli.commands.replaygain import replaygain_rescan_cmd
 from yubal.cli.commands.tags import tags_cmd
 from yubal.cli.commands.version import version_cmd
 
-__all__ = ["download_cmd", "meta_cmd", "tags_cmd", "version_cmd"]
+__all__ = [
+    "download_cmd",
+    "meta_cmd",
+    "replaygain_rescan_cmd",
+    "tags_cmd",
+    "version_cmd",
+]
